563aac092db14d720b8ecc9f89971114eb476cb2
saivaraprasad195 <62549091+saivaraprasad195@users.noreply.github.com>
2023-11-12 17:26:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let arr = [0];
+	for(let i=0;i<str.length;i++){
+		let index =str.charCodeAt(i)-97;
+		if(arr[index] == undefined) arr[index] = 1;
+		else
+		arr[index]++;
+	}
+
+	let result ='';
+
+	for(let i=0;i<str.length;i++){
+		let index =str.charCodeAt(i)-97;
+		if(arr[index] === 1){
+			result=str.charAt(i);
+			break;
+		}
+	}
+	console.log(result);
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
